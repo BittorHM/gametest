a5214b6ec50e1a3b081eef3f1da45f5ff3dcd7f7
BittorHM <61824737+BittorHM@users.noreply.github.com>
2021-11-05 09:16:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return "Hello Team"
 
+@app.route("/hello")
+def index2():
+    return "Hello Cloud"
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
