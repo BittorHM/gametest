0d8b9462e353ab6e06f9e39966e81517da4c43f0
BittorHM <61824737+BittorHM@users.noreply.github.com>
2022-03-08 10:28:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,6 @@ def index():
 def index2():
     return "Hello Cloud"
 
-@app.route("/login")
-def login():
-    return "Will login here"
-
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
